085450fa6f254e62f19b004108a76107b95f5964
Solomon Olatunji <realsolomon01@outlook.com>
2024-01-01 06:44:22 +0100

    updates

--- app/db/mongodb.py
+++ app/db/mongodb.py
@@ -1,6 +1,20 @@
+import asyncio
 import motor.motor_asyncio
 from app.core.config import settings
 
 MONGO_DETAILS = f"mongodb://{settings.DATABASE_URL}"
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
 database = client.portfolio_blog
+
+
+async def check_mongo_connection():
+    try:
+        # Send a command to the admin database to check the connection
+        result = await client.admin.command("ping")
+        print("Connected to MongoDB:", result)
+    except Exception as e:
+        print("Failed to connect to MongoDB:", e)
+
+
+# Run the function to check the connection
+asyncio.run(check_mongo_connection())
---
+++ app/services/user.py
@@ -0,0 +1,15 @@
+from typing import Annotated
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer
+from app.middlewares.auth import Jwt
+from app.services.user import get_user_by_email
+from app.schemas.user import User as UserSchema
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/auth/login")
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    payload = await Jwt.decode_access_token(token)
+    email = payload["sub"]
+    user = await get_user_by_email(email)
+    return UserSchema(**user)
